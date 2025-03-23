3033e861a141da23b5d9f05b7e33573b075fdcbe
Chintan <chintanr21@gmail.com>
2025-03-22 20:12:07 -0400

    removed some PWA dependencies

--- src/components/InstallPWAButton.tsx
+++
@@ -1,9 +0,0 @@
-
-import InstallPWA from './InstallPWA';
-
-// This is a separate component so we can import it in the Navbar component
-const InstallPWAButton = () => {
-  return <InstallPWA />;
-};
-
-export default InstallPWAButton;

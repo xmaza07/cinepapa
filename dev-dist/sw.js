2578209c89e6be34b16f17c92fbb66ca5dab5a43
Chintan <chintanr21@gmail.com>
2025-03-26 18:07:02 -0400

    911 fix

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e7681877'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.3i8a65ctnco"
+    "revision": "0.ibem7hc772g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig(({ mode }) => ({
-  base: '/flicker-dreams-factory-13/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
